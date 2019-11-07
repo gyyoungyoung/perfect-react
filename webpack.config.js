b8138839ff431bf2a7b5e03533221199f01bbf32
GuoYang <992044394@qq.com>
2019-11-07 11:04:41 +0800

    第一个react框架

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+ 
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      title: 'Set Up Project',
+      filename: "./index.html",
+      template:"./index.html"
+    })
+  ]
+};
